800606a397f08e6344b89736dec5b3f23781a4dd
Youngha Kim <yh.kim@outlook.com>
2020-03-07 18:49:52 +1100

    Building mongo db

--- src/index.js
+++ src/index.js
@@ -1,2 +1,78 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+
+const db = require('./db');
+
+// Run the server on a port specified in our .env file or port 4000
+const port = process.env.PORT || 4000;
+// Store the DB_HOST value as a variable
+const DB_HOST = process.env.DB_HOST;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID): Note
+  }
+
+  type Mutation {
+    newNote(content: String): Note
+  }
+`;
+
+//Provide resolver function for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const app = express();
+
+// Connect to the database
+db.connect(DB_HOST);
+
+// Apollo Server Setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+// app.get('/', (req, res) => res.send('Hello World!!'));
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
---
+++ src/models/note.js
@@ -0,0 +1,25 @@
+// Require the mongoose library
+const mongoose = require('mongoose');
+
+// Define the note's database schema
+const noteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// Definte the Note model with the schema
+const Note = mongoose.model('Note', noteSchema);
+
+// Export the module
+module.exports = Note;
